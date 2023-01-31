9e427a69896eeeecaa5ff6787194671ee9070884
s-y-yu <seung.yoon.yu@gmail.com>
2023-01-31 16:32:51 +0900

    handsout java

---
+++ handsout/01_java/abstrct/ChefTest.java
@@ -0,0 +1,7 @@
+package abstrct;
+
+public class ChefTest {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ handsout/01_java/er/buildup/one/Child.java
@@ -0,0 +1,8 @@
+package er.buildup.one;
+
+
+public class Child extends Parent{
+	public Child() {
+		System.out.println("Child Constructor");
+	}
+}
