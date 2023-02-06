d35f7cbf82b8c36facc12da930eeba90a3d82a8b
s-y-yu <seung.yoon.yu@gmail.com>
2023-02-06 17:38:50 +0900

    0206 알고리즘 경진대회 문제 추가

--- sandbox/src/sandbox/Main.java
+++ sandbox/src/sandbox/Main.java
@@ -1,30 +1,30 @@
 package sandbox;
 
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-	public static int zeros = 0;
-	public static int ones = 0;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		
-		for(int i=0; i<T; i++) {
-			int N = sc.nextInt();
-			fibonacci(N);
-			System.out.println(zeros+" "+ones);
-			zeros = 0;
-			ones = 0;
+		for (int test_case = 1; test_case<=T; test_case++) {
+			String M = sc.next();
+			String[] mArr = M.split("");
+
+			int flag = -1;
+			int answer = 0;
+			if(mArr[0].equals("0")) flag = 0;
+			for(int j=0; j<mArr.length; j++) {
+
+				if(flag != Integer.parseInt(mArr[j])) {
+					flag = Integer.parseInt(mArr[j]);
+					answer++;
+				}
+				
+			}
+			System.out.println("#"+test_case+" "+answer);
+			answer = 0;
 		}
-	}
-	public static int fibonacci(int n) {
-	    if (n == 0) {
-	        zeros++;
-	        return 0;
-	    } else if (n == 1) {
-	        ones++;
-	        return 1;
-	    } else {
-	        return fibonacci(n - 1) + fibonacci(n - 2);
-	    }
 	}
 }
